5579a5535413ce20632a066049b6796ef7a872f5
sajalshres <sajal.shres@gmail.com>
2021-12-04 11:35:11 +0545

    updated status json cgi spec

--- nagios/cgi/status_json.py
+++ nagios/cgi/status_json.py
@@ -6,53 +6,96 @@ class StatusJsonCgi(BaseCgi):
         super().__init__(rest_client)
         self.path = "statusjson.cgi"
 
-    def host_count(self) -> None:
-        pass
-
-    def host_list(self) -> None:
-        pass
-
-    def host(self) -> None:
-        pass
-
-    def service_count(self) -> None:
-        pass
-
-    def service_list(
-        self,
-        **params
-    ) -> None:
-        params = { "query": "servicelist", **params}
+    def host_count(self, **params) -> None:
+        params = {"query": "hostcount", **params}
         response = self.rest_client.get(path=self.path, params=params)
 
         return self._result_handler(response)
 
-    def service(self) -> None:
-        pass
+    def host_list(self, **params) -> None:
+        params = {"query": "hostlist", **params}
+        response = self.rest_client.get(path=self.path, params=params)
 
-    def comment_count(self) -> None:
-        pass
+        return self._result_handler(response)
 
-    def comment_list(self) -> None:
-        pass
+    def host(self, host_name: str) -> None:
+        params = {"query": "host", "hostname": host_name}
+        response = self.rest_client.get(path=self.path, params=params)
 
-    def comment(self) -> None:
-        pass
+        return self._result_handler(response)
 
-    def downtime_count(self) -> None:
-        pass
+    def service_count(self, **params) -> None:
+        params = {"query": "servicecount", **params}
+        response = self.rest_client.get(path=self.path, params=params)
 
-    def downtime_list(self) -> None:
-        pass
+        return self._result_handler(response)
 
-    def downtime(self) -> None:
-        pass
+    def service_list(self, **params) -> None:
+        params = {"query": "servicelist", **params}
+        response = self.rest_client.get(path=self.path, params=params)
 
-    def program_status(self) -> None:
-        pass
+        return self._result_handler(response)
 
-    def performance_data(self) -> None:
-        pass
+    def service(self, host_name: str, description: str) -> None:
+        params = {
+            "query": "service",
+            "hostname": host_name,
+            "servicedescription": description,
+        }
+        response = self.rest_client.get(path=self.path, params=params)
 
-    def help(self) -> None:
-        pass
+        return self._result_handler(response)
+
+    def comment_count(self, **params) -> None:
+        params = {"query": "commentcount", **params}
+        response = self.rest_client.get(path=self.path, params=params)
+
+        return self._result_handler(response)
+
+    def comment_list(self, **params) -> None:
+        params = {"query": "commentlist", **params}
+        response = self.rest_client.get(path=self.path, params=params)
+
+        return self._result_handler(response)
+
+    def comment(self, id: int) -> None:
+        params = {"query": "comment", "commentid": id}
+        response = self.rest_client.get(path=self.path, params=params)
+
+        return self._result_handler(response)
+
+    def downtime_count(self, **params) -> None:
+        params = {"query": "downtimecount", **params}
+        response = self.rest_client.get(path=self.path, params=params)
+
+        return self._result_handler(response)
+
+    def downtime_list(self, **params) -> None:
+        params = {"query": "downtimelist", **params}
+        response = self.rest_client.get(path=self.path, params=params)
+
+        return self._result_handler(response)
+
+    def downtime(self, id: int) -> None:
+        params = {"query": "downtime", "downtimeid": id}
+        response = self.rest_client.get(path=self.path, params=params)
+
+        return self._result_handler(response)
+
+    def program_status(self, **params) -> None:
+        params = {"query": "programstatus", **params}
+        response = self.rest_client.get(path=self.path, params=params)
+
+        return self._result_handler(response)
+
+    def performance_data(self, **params) -> None:
+        params = {"query": "performancedata", **params}
+        response = self.rest_client.get(path=self.path, params=params)
+
+        return self._result_handler(response)
+
+    def help(self, **params) -> None:
+        params = {"query": "help", **params}
+        response = self.rest_client.get(path=self.path, params=params)
+
+        return self._result_handler(response)
